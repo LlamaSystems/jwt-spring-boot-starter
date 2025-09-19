bf084b4ba89a38503043347796a306622f31399d
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-09-19 12:05:07 +0500

    package-info for jwt/*

---
+++ src/main/java/io/github/aliabbosashurov/jwt/package-info.java
@@ -0,0 +1,8 @@
+/// Package containing core JWT configuration properties and utilities.
+///
+/// All types in this package are [org.jspecify.annotations.NonNull]
+/// by default due to the [org.jspecify.annotations.NullMarked] annotation.
+@NullMarked
+package io.github.aliabbosashurov.jwt;
+
+import org.jspecify.annotations.NullMarked;
