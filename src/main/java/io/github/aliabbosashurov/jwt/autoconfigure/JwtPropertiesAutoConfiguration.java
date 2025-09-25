ff10d2c4d9a5edd90a5aec1ccb9dab2ac7ed9021
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2025-09-25 14:56:55 +0500

    migrated docs into Markdown style

--- src/main/java/io/github/aliabbosashurov/jwt/autoconfigure/JwtPropertiesAutoConfiguration.java
+++ src/main/java/io/github/aliabbosashurov/jwt/autoconfigure/JwtPropertiesAutoConfiguration.java
@@ -5,15 +5,13 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-/**
- * Auto-configuration for enabling JWT support in Spring Boot applications.
- * <p>
- * The auto-configuration is lightweight and safe to include in any Spring Boot project.
- * It ensures that JWT properties are available for injection into services or components
- * that require them.
- *
- * @author Aliabbos Ashurov
- */
+/// Auto-configuration for enabling JWT support in Spring Boot applications.
+///
+/// The auto-configuration is lightweight and safe to include in any Spring Boot project.
+/// It ensures that JWT properties are available for injection into services or components
+/// that require them.
+///
+/// @author Aliabbos Ashurov
 @AutoConfiguration
 @EnableConfigurationProperties(JwtProperties.class)
 @ConditionalOnProperty(prefix = "app.security.jwt", name = "enabled", havingValue = "true", matchIfMissing = true)
