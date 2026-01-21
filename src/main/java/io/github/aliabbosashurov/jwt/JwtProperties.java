18718725ecc76b1aedaffbe3e33c3d7f30a6f857
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2026-01-21 14:45:53 +0500

    updated doc

--- src/main/java/io/github/aliabbosashurov/jwt/JwtProperties.java
+++ src/main/java/io/github/aliabbosashurov/jwt/JwtProperties.java
@@ -11,34 +11,34 @@ import java.time.Duration;
 @ConfigurationProperties(prefix = "app.security.jwt")
 public record JwtProperties(
 
-        // Whether JWT support is enabled.
+        /// Whether JWT support is enabled.
         @DefaultValue("true")
         boolean enabled,
 
-        // Secret key for signing and verifying JWTs.
+        /// Secret key for signing and verifying JWTs.
         String secretKey,
 
-        // JWT expiration duration.
+        /// JWT expiration duration.
         @DefaultValue("60m")
         Duration expiration,
 
-        // Refresh token expiration duration.
+        /// Refresh token expiration duration.
         @DefaultValue("7d")
         Duration refreshTokenExpiration,
 
-        // Authorization header token prefix.
+        /// Authorization header token prefix.
         @DefaultValue("Bearer")
         String tokenPrefix,
 
-        //  HTTP header name.
+        ///  HTTP header name.
         @DefaultValue("Authorization")
         String header,
 
-        // JWT issuer.
+        /// JWT issuer.
         @DefaultValue("default-issuer")
         String issuer,
 
-        //  JWT audience.
+        ///  JWT audience.
         @DefaultValue("all")
         String audience
 ) {
